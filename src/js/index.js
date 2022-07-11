baf792084f015ca31f25126b6f7b9d9f19d28cdf
Mario <waro.brambilla@gmail.com>
2022-07-11 16:55:26 +0000

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,36 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faclock } from '@fortawesome/free-solid-svg-icons';
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+function SimpleCounter (props){
+    return <div className="bigCounter">
+         <div className="clock"> {props.timer}</div>
+        <div className="one">{props.digitOne}</div>
+
+    </div>;
+}
+
+let counter = 0;
+setInterval(function(){
+    const one = Math.floor(counter/1);
+    const element = <FontAwesomeIcon icon="fa-solid fa-clock" />;
+   
+    counter++;
+    ReactDOM.render(
+    <SimpleCounter 
+        digitOne={one}
+        timer={element}
+        />, 
+    document.querySelector("#app"),
+    );
+},1000)
+
+
