f3c9d859c61cdc9a601482f61ef7cf063e5127ab
Mario <waro.brambilla@gmail.com>
2022-07-13 13:21:48 +0000

    completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,37 @@
-//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faclock } from '@fortawesome/free-solid-svg-icons';
-
-
-// include your styles into the webpack bundle
+import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 import "../styles/index.css";
-
-
 
 function SimpleCounter (props){
     return <div className="bigCounter">
-         <div className="clock"> {props.timer}</div>
+         <div className="clock">{props.clock}</div>
+        <div className="four">{props.digitFour}</div>
+        <div className="three">{props.digitThree}</div>
+        <div className="two">{props.digitTwo}</div>
         <div className="one">{props.digitOne}</div>
-
     </div>;
 }
 
 let counter = 0;
 setInterval(function(){
-    const one = Math.floor(counter/1);
-    const element = <FontAwesomeIcon icon="fa-solid fa-clock" />;
-   
-    counter++;
+    const one = Math.floor(counter % 10);
+    const two = Math.floor(counter/10)%10;
+    const three = Math.floor(counter/100)%100;
+    const four = Math.floor(counter/1000)%1000;
+    const element = <FontAwesomeIcon icon={faClock} />
+     counter++;
     ReactDOM.render(
+    
     <SimpleCounter 
         digitOne={one}
-        timer={element}
-        />, 
-    document.querySelector("#app"),
+        digitTwo={two}
+        digitThree={three}
+        digitFour={four}
+        clock ={element}/>,
+        document.querySelector("#app"),
+        
     );
 },1000)
 
